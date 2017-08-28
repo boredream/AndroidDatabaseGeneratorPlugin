b7195332b9eb81a9bd61f8b5cd81bb599d2959f2
boredream <48262906@qq.com>
2017-08-28 13:35:51 +0800

    init

---
+++ src/DatabaseGenerateAction.java
@@ -0,0 +1,13 @@
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
+
+public class DatabaseGenerateAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        
+    }
+}
